e6f55356f684878837daff4eb8e96b9673f903a5
wpaltm12 <wpaltm12@naver.com>
2020-12-14 16:02:43 +0900

    CORS 필터 추가

--- src/main/java/com/config/java/ServletConfiguration.java
+++ src/main/java/com/config/java/ServletConfiguration.java
@@ -3,6 +3,10 @@ package com.config.java;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -24,5 +28,16 @@ public class ServletConfiguration extends WebMvcConfigurerAdapter {
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		return resolver;
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+
+		registry
+		.addMapping("/**")
+		.allowedOrigins("*")
+		.allowedMethods("POST", "GET")
+		.allowedHeaders("*")
+		.allowCredentials(true);
 	}
 }
